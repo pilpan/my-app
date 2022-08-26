f13f399b2abe8c183305c789802af2d06120bbaa
pilpan <plip2010195@gmail.com>
2022-08-26 15:27:21 +0300

    fix some css

--- src/components/InputNew.jsx
+++ src/components/InputNew.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 
 export default function InputNew({ el, index }) {
+  if (
+    ((index % 9 === 0 || index % 9 === 1 || index % 9 === 2) && index < 21)
+        || ((index % 9 === 6 || index % 9 === 7 || index % 9 === 8) && index < 27)
+        || ((index % 9 === 3 || index % 9 === 4 || index % 9 === 5) && (index > 27 && index < 53))
+        || ((index % 9 === 0 || index % 9 === 1 || index % 9 === 2) && index > 53)
+        || ((index % 9 === 6 || index % 9 === 7 || index % 9 === 8) && index > 53)
+  ) {
+    return (
+      <input
+        readOnly
+        value={el}
+        style={{
+          width: '50px',
+          height: '50px',
+        }}
+        className="text-center  bg-secondary"
+        id={`${index}S`}
+      />
+    );
+  }
   return (
     <input
       id={`${index}S`}
